5ffd2c6c070fe172848ddbda693baed20fd0c802
José Brissos Raposo <61153575+josebrissosraposo@users.noreply.github.com>
2023-05-28 02:33:13 +0100

    remove hardcoded link from home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ export function Home() {
   return (
     <>
       <h1>Home</h1>
-      <p>Welcome! Make sure to visit our <a href="/store">store</a>!</p>
+      <p>Welcome! Make sure to visit our store!</p>
     </>
   );
 }
